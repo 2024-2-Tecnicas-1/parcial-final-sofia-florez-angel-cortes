d026bccf4180d49e0407929c0c93dd538968f6fa
angelt.rodriguezp <angelt.rodriguezp@korandlorenz.edu.co>
2024-11-18 08:12:26 -0500

    Se añade clase Leon y Animal

--- src/main/java/logicaNegocio/Leon.java
+++ src/main/java/logicaNegocio/Leon.java
@@ -1,5 +1,24 @@
 package logicaNegocio;
 
-public class Leon {
+public abstract class Leon extends Animal {
     // TODO: Aquí va tu código
+       public Leon() {
+        this.habitat = TipoHabitat.TERRESTRE;
+    }
+ 
+    @Override
+    public String emitirSonido() {
+        return "Rugido";
+    }
+ 
+    @Override
+    public String obtenerDieta() {
+        return "Carnívoro";
+    }
+ 
+    public TipoHabitat getHabitat() {
+        return habitat;
+    }
 }
+ 
+ 
